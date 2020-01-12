0b0a81db3215585be90a1b9cf80cd9d72dbf4095
ashley kelham <neonyourself@hotmail.com>
2020-01-12 12:44:31 -0600

    added listener

--- alertlistener/main.py
+++ alertlistener/main.py
@@ -4,12 +4,21 @@ import os
 client = datastore.Client()
 
 def alerts(request):
-    key = client.key('alerts-' + os.environ['ENV'], 1234)
-    entity = datastore.Entity(key=key)
-    entity.update({
-        'foo': u'bar',
-        'baz': 1337,
-        'qux': False,
-    })
-    client.put(entity)
+    request_json = request.get_json()
+    incident = request_json['incident']
+    state = incident['state']
+    name = incident['policy_name']
+    incident_id = incident['incdent_id']
+    key = client.key('alerts-' + os.environ['ENV'], incident_id)
+
+    if state == 'open':
+        entity = datastore.Entity(key=key)
+        entity.update({
+            'state': state,
+            'name': name
+        })
+        client.put(entity)
+    else:
+        client.delete(key)
+
     return 'success'
--- slowfunction/main.py
+++ slowfunction/main.py
@@ -1,14 +1,6 @@
 import time
 
 def hello_world(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
     request_json = request.get_json()
     time.sleep(2)
     if request.args and 'message' in request.args:
